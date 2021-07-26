d0f85e7a8c5c169801eb3ff0f53da41524819d72
spidergon <spidergon@gmail.com>
2021-07-26 19:31:07 -0300

    corrected mailer

--- src/utils/api/mail.js
+++ src/utils/api/mail.js
@@ -1,16 +1,19 @@
 import nodemailer from 'nodemailer';
+import smtpTransport from 'nodemailer-smtp-transport';
 import { htmlToText } from 'html-to-text';
 import { mailEnv } from './env';
 import purify from '@utils/purify';
 
 const { host, port, user, pass, from } = mailEnv;
 
-const transport = nodemailer.createTransport({
-  host,
-  port,
-  secure: Number(port) === 465,
-  auth: { user, pass },
-});
+const transport = nodemailer.createTransport(
+  smtpTransport({
+    host,
+    port,
+    secure: Number(port) === 465,
+    auth: { user, pass },
+  })
+);
 
 transport.verify((error) => {
   if (error) console.error('SMTP conf error: ', error);
