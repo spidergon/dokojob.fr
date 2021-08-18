fb49df1e683ef2c40e39ce62fc6fdf2b34f81628
spidergon <spidergon@gmail.com>
2021-08-18 09:31:16 -0300

    showing loading jobs on home page

--- src/components/home/jobs.js
+++ src/components/home/jobs.js
@@ -10,6 +10,7 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function Jobs() {
   const [jobs, setJobs] = useState([]);
+  const [allJobs, setAllJobs] = useState([]);
   const [step, setStep] = useState(0);
   const [pages, setPages] = useState(1);
   const [loading, setLoading] = useState(true);
@@ -21,6 +22,7 @@ export default function Jobs() {
   useEffect(() => {
     if (data?.jobs) {
       setJobs(data.jobs);
+      setAllJobs(data.jobs);
       setLoading(false);
     } else {
       setLoading(true);
@@ -39,64 +41,80 @@ export default function Jobs() {
     }
   }, [step]);
 
-  if (error) return <div>Erreur de chargement !</div>;
-  if (loading) return <div>Chargement...</div>;
-
   return (
     <section id="top-anchor">
       <div className="container">
-        <Filter allJobs={data.jobs} jobs={jobs} setJobs={setJobs} />
-        {step > 0 && (
-          <button
-            style={{ marginBottom: '2em', padding: 'initial' }}
-            title="Retourner à la page précédente"
-            onClick={() => setStep(step - 1)}
-          >
-            ⬅ page précédente
-          </button>
+        <Filter allJobs={allJobs} jobs={jobs} setJobs={setJobs} />
+
+        {loading && !error && (
+          <>
+            {[0, 1, 2, 3, 4].map((i) => (
+              <div key={i} className="placeholder"></div>
+            ))}
+          </>
         )}
-        {jobs.slice(step * PER_PAGE, step * PER_PAGE + PER_PAGE).map((job, index) => {
-          return <JobItem key={index} job={job} />;
-        })}
-        <div className="pagination center">
-          <p>
-            {(pages !== 0 && (
-              <>
-                Page {step + 1} / {pages}
-              </>
-            )) || <strong>Aucun résultat, veuillez modifier votre sélection.</strong>}
+
+        {error && (
+          <p className="error center">
+            Une erreur interne est survenue. Veuillez réessayer ultérieurement.
           </p>
-          {pages > 1 && (
-            <div className="buttons flex">
+        )}
+
+        {!loading && (
+          <>
+            {step > 0 && (
               <button
-                disabled={step === 0}
-                title="Page précédente"
-                onClick={() => {
-                  canScroll.current = true;
-                  setStep(step - 1);
-                }}
+                style={{ marginBottom: '2em', padding: 'initial' }}
+                title="Retourner à la page précédente"
+                onClick={() => setStep(step - 1)}
               >
-                <svg aria-hidden="true" focusable="false" viewBox="0 0 24 24">
-                  <path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z" />
-                </svg>
-                Précédent
+                ⬅ page précédente
               </button>
-              <button
-                disabled={step === pages - 1}
-                title="Page suivante"
-                onClick={() => {
-                  canScroll.current = true;
-                  setStep(step + 1);
-                }}
-              >
-                Suivant
-                <svg aria-hidden="true" focusable="false" viewBox="0 0 24 24">
-                  <path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z" />
-                </svg>
-              </button>
+            )}
+            {jobs.slice(step * PER_PAGE, step * PER_PAGE + PER_PAGE).map((job, index) => (
+              <JobItem key={`${index}${job.id}`} job={job} />
+            ))}
+            <div className="pagination center">
+              <p>
+                {(pages !== 0 && (
+                  <>
+                    Page {step + 1} / {pages}
+                  </>
+                )) || <strong>Aucun résultat, veuillez modifier votre sélection.</strong>}
+              </p>
+              {pages > 1 && (
+                <div className="buttons flex">
+                  <button
+                    disabled={step === 0}
+                    title="Page précédente"
+                    onClick={() => {
+                      canScroll.current = true;
+                      setStep(step - 1);
+                    }}
+                  >
+                    <svg aria-hidden="true" focusable="false" viewBox="0 0 24 24">
+                      <path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z" />
+                    </svg>
+                    Précédent
+                  </button>
+                  <button
+                    disabled={step === pages - 1}
+                    title="Page suivante"
+                    onClick={() => {
+                      canScroll.current = true;
+                      setStep(step + 1);
+                    }}
+                  >
+                    Suivant
+                    <svg aria-hidden="true" focusable="false" viewBox="0 0 24 24">
+                      <path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z" />
+                    </svg>
+                  </button>
+                </div>
+              )}
             </div>
-          )}
-        </div>
+          </>
+        )}
       </div>
 
       <style jsx>{`
@@ -125,6 +143,29 @@ export default function Jobs() {
         }
         .buttons button[disabled] svg {
           fill: rgba(0, 0, 0, 0.3);
+        }
+        .placeholder {
+          height: 100px;
+          border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+          margin-bottom: 1em;
+          --shine: white;
+          --background: #efefef;
+          background-image: linear-gradient(
+            90deg,
+            var(--background) 0px,
+            var(--shine) 40px,
+            var(--background) 80px
+          );
+          background-size: 500px;
+          animation: shine 1s infinite linear;
+        }
+        @keyframes shine {
+          from {
+            background-position: 200%;
+          }
+          to {
+            background-position: -40px;
+          }
         }
         @media (min-width: 601px) {
           section {
