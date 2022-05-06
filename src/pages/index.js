7353bedd736f28b00f91a3c4fd61d91de477219e
spidergon <spidergon@gmail.com>
2022-05-06 20:25:52 -0300

    updates and disabled carbonbadge

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 import Layout from '@components/layout';
 import Hero from '@components/home/hero';
 import Jobs from '@components/home/jobs';
-import CarbonBadge from '@components/carbonBadge';
+// import CarbonBadge from '@components/carbonBadge';
 
 export default function Home() {
   return (
     <Layout title="Votre emploi numérique en Outre-mer">
       <Hero />
       <Jobs />
-      <CarbonBadge />
+      {/* <CarbonBadge /> */}
     </Layout>
   );
 }
