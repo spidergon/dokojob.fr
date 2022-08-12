8308a0dc56c1ebc47b93ab527a20d5f1fcfe0523
spidergon <spidergon@gmail.com>
2022-08-12 16:56:24 -0300

    updates and corrections

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['entreprise.pole-emploi.fr', 'res.cloudinary.com'],
+  },
+};
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({
-  images: {
-    domains: ['entreprise.pole-emploi.fr', 'res.cloudinary.com'],
-  },
-  swcMinify: true,
-});
+module.exports = withBundleAnalyzer(nextConfig);
+
+// module.exports = withBundleAnalyzer({
+//   images: {
+//     domains: ['entreprise.pole-emploi.fr', 'res.cloudinary.com'],
+//   },
+//   swcMinify: true,
+// });
