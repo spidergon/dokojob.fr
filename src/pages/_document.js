a0ef45e3c76fb6878cc1c4f48c3887bc7bfece36
Chris <cservius@proton.me>
2023-03-22 11:55:07 -0300

    updates

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="fr" prefix="og: https://ogp.me/ns#">
+      <Html lang="fr">
         <Head />
         <body>
           <Main />
